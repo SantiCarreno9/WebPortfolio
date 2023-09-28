cc7c529406930551d5b388784c41b6dd75b4c329
SantiCarreno9 <santiago.carreno05@gmail.com>
2023-09-28 19:47:13 -0400

    Unnecessary objects were deleted

--- config/express.js
+++ config/express.js
@@ -5,21 +5,12 @@
     09/28/2023
 */
 const express = require('express');
-const bodyParser = require('body-parser');
-const methodOverride = require('method-override');
 
 const appFunction = () => {
     const app = express();
-
-    //Used mainly for post request
-    app.use(bodyParser.urlencoded({
-        extended: true
-    }));
-    app.use(bodyParser.json());
-    app.use(methodOverride());//Using middleware for all incoming requests
     app.set('views', './app/views');
     app.set('view engine', 'ejs');
-    app.use('/', require('../app/routes/index.server.routes.js'));//Any route that starts with '/' redirect it to that route
+    app.use('/', require('../app/routes/index.server.routes.js'));
     app.use(express.static('./public'));
     app.use(express.static('./node_modules'));
 
