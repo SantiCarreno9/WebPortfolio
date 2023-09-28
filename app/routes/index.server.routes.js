46fe94bf7b9484bf7a2837988a87362924420860
SantiCarreno9 <santiago.carreno05@gmail.com>
2023-09-28 16:47:07 -0400

    Header and footer partials, logo and NavBar added

--- app/routes/index.server.routes.js
+++ app/routes/index.server.routes.js
@@ -1,24 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
+//Home page route
 router.get('/', (req,res,next)=>{
-    res.render('index',{title: 'Home'});
+    res.render('home',{title: 'Home'});
 });
 
-router.get('/about', (req,res,next)=>{
-    res.render('about',{title: 'About Me'});
-});
-
+//Projects page route
 router.get('/projects', (req,res,next)=>{
     res.render('projects',{title: 'Projects'});
 });
 
-router.get('/contact', (req,res,next)=>{
-    res.render('contact',{title: 'Contact Me'});
+//Services page route
+router.get('/services', (req,res,next)=>{
+    res.render('services',{title: 'Services'});
 });
 
-router.get('/services', (req,res,next)=>{
-    res.render('Services',{title: 'Services'});
+//About page route
+router.get('/about', (req,res,next)=>{
+    res.render('about',{title: 'About'});
+});
+
+//Contact page route
+router.get('/contact', (req,res,next)=>{
+    res.render('contact',{title: 'Contact'});
 });
 
 module.exports = router;
