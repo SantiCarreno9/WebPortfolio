c0bd7429f118ae5328decd1bfd7a99bdcbf99242
SantiCarreno9 <santiago.carreno05@gmail.com>
2023-09-29 00:32:06 -0400

    Unnecessary lines were deleted

--- server.js
+++ server.js
@@ -4,10 +4,9 @@
     301283698
     09/28/2023
 */
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';//Here it will take the first value that is assigned
 const express = require('./config/express');
 const app = express();
 app.listen(3000);
-module.exports = app;//Done to avoid writing the path to the module
+module.exports = app;
 
 console.log('Server running at http://localhost:3000/');
